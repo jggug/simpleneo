a94f1359749c151eebba5efdd800c550e98a3385
Tsuyoshi Yamamoto <mottsnite@gmail.com>
2010-10-10 18:13:04 +0900

    1.3.5からtarget/workを指定するとコマンドの挙動がおかしくなるので、コメントアウト

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,7 +3,7 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.plugins.dir="target/plugins"
-grails.project.work.dir="target/work"
+//grails.project.work.dir="target/work"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
